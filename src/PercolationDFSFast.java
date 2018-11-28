fe7dce7e6be476773063e9b7ff329294cc5f01b5
Raksha git config --global user.name Raksha <rsd35@duke.edu>
2018-11-28 13:52:38 -0500

    fixed?

--- src/PercolationDFSFast.java
+++ src/PercolationDFSFast.java
@@ -1,7 +1,7 @@
 
 public class PercolationDFSFast extends PercolationDFS{
 	/**
-	 * super constructur extended from PercolationDFS
+	 * super constructor extended from PercolationDFS
 	 * @param n an integer used a initialize a grid of size n X n
 	 */
 	public PercolationDFSFast(int n) {
@@ -12,10 +12,11 @@ public class PercolationDFSFast extends PercolationDFS{
 	 */
 	@Override
 	protected void updateOnOpen(int row, int col) {
-	if (row==0 || inBounds(row -1 , col) && isFull(row - 1, col)
-			|| inBounds(row, col -1) && isFull(row, col -1)
-			|| inBounds(row, col +1) && isFull(row, col + 1)
-			|| inBounds(row +1, col) && isFull(row +1, col)){
+	if (row==0 && isOpen(row, col) && !isFull(row, col)
+			|| inBounds(row -1 , col) && isOpen(row-1, col) && !isFull(row - 1, col)
+			|| inBounds(row, col -1) && isOpen(row, col-1) && !isFull(row, col -1)
+			|| inBounds(row, col +1) && isOpen(row, col +1) && !isFull(row, col + 1)
+			|| inBounds(row +1, col) && isOpen(row+1, col) && !isFull(row +1, col)){
 
 				dfs(row, col);
 		}
