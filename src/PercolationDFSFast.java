708e99bdaae03730c1f8cd81928922e7fabfc9bc
Raksha git config --global user.name Raksha <rsd35@duke.edu>
2018-11-28 14:03:26 -0500

    pls work

--- src/PercolationDFSFast.java
+++ src/PercolationDFSFast.java
@@ -12,11 +12,12 @@ public class PercolationDFSFast extends PercolationDFS{
 	 */
 	@Override
 	protected void updateOnOpen(int row, int col) {
-	if (row==0 && isOpen(row, col) && !isFull(row, col)
-			|| inBounds(row -1 , col) && isOpen(row-1, col) && !isFull(row - 1, col)
-			|| inBounds(row, col -1) && isOpen(row, col-1) && !isFull(row, col -1)
-			|| inBounds(row, col +1) && isOpen(row, col +1) && !isFull(row, col + 1)
-			|| inBounds(row +1, col) && isOpen(row+1, col) && !isFull(row +1, col)){
+	//if (isFull(row,col)) return;
+	if (row==0 && !isFull(row, col)
+			|| inBounds(row -1 , col)  && isFull(row - 1, col)
+			|| inBounds(row, col -1) && isFull(row, col -1)
+			|| inBounds(row, col +1) && isFull(row, col + 1)
+			|| inBounds(row +1, col) && isFull(row +1, col)){
 
 				dfs(row, col);
 		}
